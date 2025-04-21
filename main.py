c44d0323dff57fa2fa372f795defa63c5d6863b3
samrasiddiqui1 <samrasiddiquiofficial@gmail.com>
2025-04-21 14:28:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def main():
+ num1 = int(input('Enter your first number: '))
+ num2 = int(input('Enter your second number: '))
+ total = num1 + num2
+ print(f'the sum of {num1}+{num2} is  {total}')
+
+if __name__ == "__main__":
+    main()
+     
+     
